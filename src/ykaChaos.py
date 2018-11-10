3c21244b87bd290bf326f22b3ac2035d215f9ab5
Seong Yul Kim <yul2ya@nate.com>
2018-11-10 23:00:42 +0900

    rename variables

--- src/ykaChaos.py
+++ src/ykaChaos.py
@@ -11,34 +11,35 @@ class Yka(QtWidgets.QMainWindow):
         QtWidgets.QMainWindow.__init__(self)
         self.ui = uic.loadUi("../ui/yka.ui")
         self.ui.show()
-        self.ui.pushButton.clicked.connect(self.serv)
-        self.ui.pushButton_2.clicked.connect(self.cli)
-        self.ui.pushButton_3.clicked.connect(self.send_msg)
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ui.startServer.clicked.connect(self.start_server)
+        self.ui.connectServer.clicked.connect(self.connect_server)
+        self.ui.send.clicked.connect(self.send_msg)
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     def send_msg(self):
-        self.s.sendall(self.ui.lineEdit_5.Text().encoding())
+        print(self.ui.inputMessage.text())
+        self.socket.sendall(self.ui.inputMessage.text().encode())
 
-    def serv(self):
-
-        self.s.bind(('localhost', 5000))
-        print("연결완료")
-        t1 = threading.Thread(target=self.th1)
+    def start_server(self):
+        self.socket.bind(('localhost', 5000))
+        t1 = threading.Thread(target=self.socket_listening_thread)
         t1.daemon = True
         t1.start()
+        print("서버준비완료")
 
-    def cli(self):
-        self.s.connect(('localhost', 5000))
+    def connect_server(self):
+        self.socket.connect(('localhost', 5000))
+        print("서버연결시도")
 
-    def th1(self):
-        self.s.listen(1)
-        conn, addr = self.s.accept()
+    def socket_listening_thread(self):
+        self.socket.listen(1)
+        conn, addr = self.socket.accept()
         while True:
             msg = conn.recv(1024).decode()
             if msg:
-                self.ui.plainTextEdit.appendPlainText(msg)
+                self.ui.messageViewer.appendPlainText(msg)
                 print(msg)
-            time.sleep(1)
+            time.sleep(0.3)
 
 
 if __name__ == '__main__':
